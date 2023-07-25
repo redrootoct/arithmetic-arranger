7fa5d32e82e1166202c3c516ebbb79d622d1b519
redrootoct <60930962+redrootoct@users.noreply.github.com>
2023-07-25 15:22:03 +0300

    Update arithmetic_arranger.py
    My Version

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,59 +1,50 @@
-def arithmetic_arranger(problems, result=False):
-    """
-    Receives a list of strings that are arithmetic problems and 
-    returns the problems arranged vertically and side-by-side. 
-    If result is set to True, the answers are displayed as well.
+def arithmetic_arranger(problems, B = None):
+    my_str = problems
+    line1=""
+    line2=""
+    line3=""
+    line4=""
+    tab1=0
+    tab2=0
+    i=0
 
-    Possible errors:
-        `Error: Too many problems.`: More than 5 problems supplied to the function.
-        `Error: Operator must be '+' or '-'.`: The appropriate operators the function accepts are addition (+) and subtraction (-). 
-                Multiplication and division will return an error. 
-                Other operators not mentioned in this bullet point are not included.
-        `Error: Numbers must only contain digits.`: Each number (operand) should only contain digits.
-        `Error: Numbers cannot be more than four digits.`: Each operand (aka number on each side of the operator) has a max of four digits in width.
-    """
-    if len(problems) > 5:
+    if len(my_str) <= 5:
+
+        for st in my_str:
+            x=st.split()
+            if len(x[0]) < 5 and len(x[2]) < 5:
+                if x[1] in "+-":
+                    if x[0].isnumeric() and x[2].isnumeric():
+                        tab1=len(x[0])
+                        tab2=len(x[2])
+                        v = max(len(x[0]),len(x[2]))
+                        lines="-"
+                        line1=line1 + x[0].rjust(v+2,' ')
+                        line2=line2 + x[1] + x[2].rjust(v+1,' ')
+                        line3=line3 + lines*(v+2)
+                        if x[1]=="+":
+                            summa=int(x[0])+int(x[2])
+                        else:
+                            summa=int(x[0])-int(x[2])
+                        line4=line4 + str(summa).rjust(v+2,' ')
+
+                        i=i+1
+                        if i<len(my_str):
+                            line1=line1 + '    '
+                            line2=line2 + '    '
+                            line3=line3 + '    '
+                            line4=line4 + '    '
+                    else:
+                            return "Error: Numbers must only contain digits."
+                else:
+                    return "Error: Operator must be '+' or '-'."
+            else:
+                return "Error: Numbers cannot be more than four digits."
+    else:
         return "Error: Too many problems."
-
-    ops = {
-        '+': lambda pair: str(pair[0] + pair[1]),
-        '-': lambda pair: str(pair[0] - pair[1]),
-    }
-    arranged_problems = []
-    top = []
-    bottom = []
-    lines = []
-    results = []
-
-    for problem in problems:
-        chunks = problem.split()
-        max_len = len(max(chunks, key=len))
-
-        if not all([i.isnumeric() for i in chunks[::2]]):
-            return "Error: Numbers must only contain digits."
-        elif chunks[1] not in ops.keys():
-            return "Error: Operator must be '+' or '-'."
-        elif max_len > 4:
-            return "Error: Numbers cannot be more than four digits."
-
-        line_len = max_len + 2
-
-        line = '-' * line_len  # 2 corresponds to the sign and space
-        first_num = chunks[0].rjust(line_len, ' ')
-        second_num = f"{chunks[1]}{' ' * (line_len - len(chunks[2]) - 1)}{chunks[2]}"
-
-        top.append(first_num)
-        bottom.append(second_num)
-        lines.append(line)
-
-        if result:
-            res = ops[chunks[1]]([int(i) for i in chunks[::2]])
-            results.append(f"{res.rjust(line_len, ' ')}")
-
-    arranged_problems = '\n'.join(['    '.join(i)
-                                   for i in (top, bottom, lines)])
-
-    if results:
-        arranged_problems += '\n' + '    '.join(results)
-
-    return arranged_problems
+    if B == True:
+        answer=line1+"\n"+line2+"\n"+line3+"\n"+line4
+        return answer
+    else:
+        answer=line1+"\n"+line2+"\n"+line3
+        return answer
