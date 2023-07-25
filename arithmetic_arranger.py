d5d2e1a7c44e093c99f48724e3b330edde1641d9
redrootoct <60930962+redrootoct@users.noreply.github.com>
2023-07-25 16:16:31 +0300

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -4,37 +4,30 @@ def arithmetic_arranger(problems, B = None):
     line2=""
     line3=""
     line4=""
-    tab1=0
-    tab2=0
     i=0
-
     if len(my_str) <= 5:
-
         for st in my_str:
             x=st.split()
             if len(x[0]) < 5 and len(x[2]) < 5:
                 if x[1] in "+-":
                     if x[0].isnumeric() and x[2].isnumeric():
-                        tab1=len(x[0])
-                        tab2=len(x[2])
                         v = max(len(x[0]),len(x[2]))
                         lines="-"
-                        line1=line1 + x[0].rjust(v+2,' ')
-                        line2=line2 + x[1] + x[2].rjust(v+1,' ')
-                        line3=line3 + lines*(v+2)
+                        line1+= x[0].rjust(v+2,' ')
+                        line2+= x[1] + x[2].rjust(v+1,' ')
+                        line3+= lines*(v+2)
                         if x[1]=="+":
                             summa=int(x[0])+int(x[2])
                         else:
                             summa=int(x[0])-int(x[2])
-                        line4=line4 + str(summa).rjust(v+2,' ')
-
+                        line4+= str(summa).rjust(v+2,' ')
                         i=i+1
                         if i<len(my_str):
-                            line1=line1 + '    '
-                            line2=line2 + '    '
-                            line3=line3 + '    '
-                            line4=line4 + '    '
-                    else:
+                            line1+='    '
+                            line2+='    '
+                            line3+='    '
+                            line4+='    '
+                    else: 
                             return "Error: Numbers must only contain digits."
                 else:
                     return "Error: Operator must be '+' or '-'."
